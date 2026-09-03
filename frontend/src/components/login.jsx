d5e3c9f38e265b5f46ed58efb3ecdbf80806a621
Suleman Raza <sulemanraza193@gmail.com>
2026-09-03 17:23:37 +0500

    Update API_URL to use environment variable

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 function Login({ onLogin }) {
   const [email, setEmail] = useState("");
